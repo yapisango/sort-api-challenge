b2d05bd77284a04f274311966866c97230866e48
unknown <yapisango@gmail.com>
2025-06-15 22:17:48 +0200

    Ready for Render deployment

--- README.md
+++ README.md
@@ -20,3 +20,20 @@ This project is a simple Node.js API that sorts arrays of numbers.
 ```bash
 git clone https://github.com/yapisango/sort-api-challenge.git
 cd sort-api-challenge
+
+### Install Dependencies
+
+npm install
+
+node index.js
+
+API runs on: http://localhost:8000
+
+curl -X POST http://localhost:8000/sort \
+  -H "Content-Type: application/json" \
+  -d '{"numbers": [9, 2, 5], "order": "asc"}'
+
+  {
+  "sorted": [2, 5, 9]
+}
+
--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://sort-api-frontend-q09qcztvh-sango-mabhuti-yapis-projects.vercel.app'
+  ]
+}));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../public')));
+
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
 
 app.post('/', (req, res) => {
   const { data } = req.body;
@@ -10,8 +25,9 @@ app.post('/', (req, res) => {
     return res.status(400).json({ error: 'Invalid input. "data" must be a string.' });
   }
 
-  const sortedChars = data.split('').sort();
+  const sortedChars = data.split('').sort().join('');
   res.status(200).json({ word: sortedChars });
 });
 
 module.exports = app;
+
